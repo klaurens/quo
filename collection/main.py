ed0bc87a35fb7c164e6b952e4644879953b5885b
klaurens <50029982+klaurens@users.noreply.github.com>
2024-10-23 02:12:01 -0500

    Scraper (#7)
    * product listing unifier
    
    * image scraper
    
    * refactor and parallelize
    
    * minor fix
    
    * logging
    
    * minor fix
    
    ---------
    
    Co-authored-by: Kenji Laurens <kenjilaurens@Kenjis-MacBook-Air.local>

--- collection/main.py
+++ collection/main.py
@@ -1,22 +1,157 @@
-from tokped_scraper import get_brand_listing, get_product_details_query
-from datetime import datetime
+from tokped_scraper import get_brand_listing, get_item_details, get_image
+from datetime import datetime, timedelta
 import os
+import glob
 import json
+from jsonpath_ng import parse
+from logger import logger
+import concurrent.futures
+from typing import List
+from functools import partial
+import time
 
-if __name__ == '__main__':
-    WRITE_DATE = datetime.today().date()
 
-    with open('brands.txt', 'r') as read_file:
-        brands = read_file.readlines()
-        brands = [b.strip() for b in brands]
+# Configuration constants
+IMAGE_MIN_BYTES = 5000
+WRITE_DATE = datetime.today().date()
+UNIFIED_FILE = f"unified/{WRITE_DATE}.json"
+SOURCE_FILE = "brands.txt"
+DETAILS_DIR = "details"
+LISTING_DIR = "listing"
+UNIFIED_DIR = "unified"
+
+
+def create_dir_if_not_exists(directory: str):
+    """Create a directory if it doesn't exist."""
+    os.makedirs(directory, exist_ok=True)
+
+
+def read_brands(source_file: str) -> List[str]:
+    """Reads brand names from a file."""
+    with open(source_file, "r") as read_file:
+        return [b.strip() for b in read_file.readlines()]
+
+
+def save_json(data: dict, file_path: str):
+    """Saves data to a JSON file."""
+    create_dir_if_not_exists(os.path.dirname(file_path))
+    with open(file_path, "w") as write_file:
+        json.dump(data, write_file)
+    logger.info(f"Saved data to {file_path}")
+
+
+def scrape_listing(source_file: str = SOURCE_FILE):
+    """Scrapes all listings from brands."""
+    brands = read_brands(source_file)
 
     for brand in brands:
-        print(f'Fetching brand list for {brand}')
-        brand_listing = get_brand_listing(brand_uri=brand)
+        try:
+            logger.info(f"Fetching brand list for {brand}")
+            brand_listing = get_brand_listing(brand_uri=brand)
 
-        file_name = f'{brand}_listing_{WRITE_DATE}'
-        file_dir = f'{brand}/{file_name}.json'
-        os.makedirs(os.path.dirname(file_dir), exist_ok=True)
-        print(f'Writing {file_name} list at {file_dir}')
-        with open(file_dir, 'w+') as write_file:
-            json.dump(brand_listing, write_file)
+            file_path = f"{LISTING_DIR}/{brand}/listing_{WRITE_DATE}.json"
+            save_json(brand_listing, file_path)
+        except Exception as e:
+            logger.error(f"Error scraping brand {brand}: {str(e)}")
+
+
+def unify_listings():
+    """Unifies and simplifies JSON product listings."""
+    scraped_products = []
+    json_files = glob.glob(f"{LISTING_DIR}/**/*.json")
+    parser = parse("$..GetShopProduct.data")
+
+    for json_file in json_files:
+        try:
+            with open(json_file, "r") as jf:
+                data = json.load(jf)
+            brand = json_file.split("/")[1]
+
+            # Insert brand into the JSON object
+            brand_parser = parse("$..GetShopProduct.data[*].brand")
+            brand_parser.update_or_create(data, brand)
+
+            values = parser.find(data)
+            scraped_products.extend([v.value for v in values])
+        except Exception as e:
+            logger.error(f"Error processing file {json_file}: {str(e)}")
+
+    save_json(scraped_products, UNIFIED_FILE)
+
+
+def scrape_product_details(product: dict):
+    """Scrapes product details for a single product."""
+    try:
+        product_name = product["name"]
+        product_brand = product["brand"]
+        product_url = product["product_url"]
+
+        product_details = get_item_details(product_url)
+        file_path = f"{DETAILS_DIR}/{product_brand}/{product_name}/{WRITE_DATE}.json"
+        save_json(product_details, file_path)
+    except Exception as e:
+        logger.error(f"Error scraping product {product['name']}: {str(e)}")
+
+
+def scrape_products():
+    """Scrapes details for all products in the unified file."""
+    latest_unified_json = sorted(glob.glob(f"{UNIFIED_DIR}/*.json"))[-1]
+
+    with open(latest_unified_json, "r") as f:
+        products = json.load(f)
+        products = [product for product_list in products for product in product_list]
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        executor.map(scrape_product_details, products)
+
+
+def fetch_image(link, dir_parts):
+    """Fetch and write image file"""
+    image_name = link.split("/")[-1]
+    product_brand = dir_parts[1]
+    product_name = dir_parts[2]
+
+    file_path = f"{DETAILS_DIR}/{product_brand}/{product_name}/{image_name}"
+    image_file = get_image(link, brand_uri=product_brand)
+    if len(image_file) >= IMAGE_MIN_BYTES:
+        with open(file_path, "wb") as f:
+            f.write(image_file)
+        logger.info(f"Image saved at {file_path}")
+    else:
+        logger.info(f"Skipped saving image: {link} (too small/broken image)")
+
+
+def scrape_images():
+    """Scrapes images from product details."""
+    json_files = glob.glob(f"{DETAILS_DIR}/**/**/*.json")
+    parser = parse("$..urlMaxRes")
+
+    for json_file in json_files:
+        try:
+            with open(json_file, "r") as jf:
+                data = json.load(jf)
+            links = [link.value for link in parser.find(data)]
+
+            dir_parts = json_file.split("/")
+            with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+                executor.map(partial(fetch_image, dir_parts=dir_parts), links)
+
+        except Exception as e:
+            logger.error(f"Error processing images for {json_file}: {str(e)}")
+
+
+if __name__ == "__main__":
+    try:
+        start_time = time.time()
+        logger.info("Start Scraping Run")
+
+        scrape_listing()
+        unify_listings()
+        scrape_products()
+        scrape_images()
+
+        end_time = time.time()
+        logger.info("Finished Scraping Run")
+        logger.info(f"Elapsed time {timedelta(seconds=end_time - start_time)}")
+    except Exception as e:
+        logger.error(f"Error during execution: {str(e)}")
--- collection/tokped_scraper.py
+++ collection/tokped_scraper.py
@@ -1,19 +1,22 @@
 import requests
 import re
+from logger import logger
+from requests.exceptions import RequestException
 from queries import get_product_query, get_sid_query, get_product_details_query
 
+# Configuration constants
 ARRAY_REQUEST_URI = "https://gql.tokopedia.com/graphql/ShopProducts"
 PDP_REQUEST_URI = "https://gql.tokopedia.com/graphql/PDPGetLayoutQuery"
-REGEX_PRODUCT_URI = "(https?://www\.)?tokopedia\.com/([\w\d]+?)/([\w\d-]+)\??"
+REGEX_PRODUCT_URI = r"(https?://www\.)?tokopedia\.com/([\w\d]+?)/([\w\d-]+)\??"
+REQUEST_TIMEOUT = 10  # Timeout for all requests (in seconds)
 
 
 def get_headers(brand_uri, product_uri=None):
-    if product_uri:
-        referer = (
-            f"https://www.tokopedia.com/{brand_uri}/{product_uri}?extParam=src%3Dshop"
-        )
-    else:
-        referer = f"https://www.tokopedia.com/{brand_uri}/product/page/1"
+    referer = (
+        f"https://www.tokopedia.com/{brand_uri}/{product_uri}?extParam=src%3Dshop"
+        if product_uri
+        else f"https://www.tokopedia.com/{brand_uri}/product/page/1"
+    )
 
     headers = {
         "sec-ch-ua": '"Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"',
@@ -30,94 +33,131 @@ def get_headers(brand_uri, product_uri=None):
     }
 
     if product_uri:
-        headers['X-TKPD-AKAMAI'] = 'pdpGetLayout'
+        headers["X-TKPD-AKAMAI"] = "pdpGetLayout"
 
     return headers
 
 
 def get_brand_sid(brand_uri):
     """Converts Brand Official Store Name to SID"""
+    try:
+        headers = get_headers(brand_uri)
+        data = [
+            {
+                "operationName": "ShopInfoCore",
+                "variables": {"domain": brand_uri, "id": 0},
+                "query": get_sid_query(),
+            }
+        ]
 
-    headers = get_headers(brand_uri)
-    data = [
-        {
-            "operationName": "ShopInfoCore",
-            "variables": {"domain": f"{brand_uri}", "id": 0},
-            "query": get_sid_query(),
-        }
-    ]
+        response = requests.post(
+            ARRAY_REQUEST_URI, headers=headers, json=data, timeout=REQUEST_TIMEOUT
+        )
+        response.raise_for_status()  # Raise an error for bad HTTP status codes
 
-    response = requests.post(ARRAY_REQUEST_URI, headers=headers, json=data)
-    sid = response.json()[0]["data"]["shopInfoByID"]["result"][0]["shopCore"]["shopID"]
-    return sid
+        sid = response.json()[0]["data"]["shopInfoByID"]["result"][0]["shopCore"][
+            "shopID"
+        ]
+        logger.info(f"Successfully retrieved SID for {brand_uri}: {sid}")
+        return sid
+    except (RequestException, KeyError) as e:
+        logger.error(f"Failed to retrieve SID for {brand_uri}: {str(e)}")
+        return None
 
 
 def get_brand_listing(brand_uri):
     """Returns all products from a brand page"""
-
     sid = get_brand_sid(brand_uri)
+    if not sid:
+        return []
 
     headers = get_headers(brand_uri)
-
     listings = []
     i = 1
+
     while True:
-        print(f"Collecting {brand_uri} page {i}")
-        data = [
-            {
-                "operationName": "ShopProducts",
-                "variables": {
-                    "source": "shop",
-                    "sid": f"{sid}",
-                    "page": i,
-                    "perPage": 80,
-                    "etalaseId": "etalase",
-                    "sort": 1,
-                },
-                "query": get_product_query(),
-            }
-        ]
+        try:
+            data = [
+                {
+                    "operationName": "ShopProducts",
+                    "variables": {
+                        "source": "shop",
+                        "sid": sid,
+                        "page": i,
+                        "perPage": 80,
+                        "etalaseId": "etalase",
+                        "sort": 1,
+                    },
+                    "query": get_product_query(),
+                }
+            ]
 
-        response = requests.post(ARRAY_REQUEST_URI, headers=headers, json=data)
+            response = requests.post(
+                ARRAY_REQUEST_URI, headers=headers, json=data, timeout=REQUEST_TIMEOUT
+            )
+            response.raise_for_status()
 
-        # Output the response from the request
-        listings.append(response.json())
-        i += 1
-        if response.json()[0]["data"]["GetShopProduct"]["links"]["next"] == "":
-            return listings
+            listings.append(response.json())
+
+            if not response.json()[0]["data"]["GetShopProduct"]["links"]["next"]:
+                break
+            i += 1
+        except (RequestException, KeyError) as e:
+            logger.error(
+                f"Error retrieving product listings for {brand_uri} on page {i}: {str(e)}"
+            )
+            break
+
+    logger.info(f"Retrieved {len(listings)} pages of products for {brand_uri}")
+    return listings
 
 
 def split_product_uri(product_url):
     match = re.match(REGEX_PRODUCT_URI, product_url)
     if not match:
-        raise Exception(f"{product_url} does not match product listing regex condition")
-    else:
-        brand_uri, product_uri = match[2], match[3]
-        return brand_uri, product_uri
+        raise ValueError(f"Invalid product URL: {product_url}")
+
+    brand_uri, product_uri = match[2], match[3]
+    return brand_uri, product_uri
 
 
 def get_item_details(product_url):
-    brand_uri, product_uri = split_product_uri(product_url)
-    headers = get_headers(brand_uri, product_uri=product_uri)
+    try:
+        brand_uri, product_uri = split_product_uri(product_url)
+        headers = get_headers(brand_uri, product_uri)
 
-    data = [
-        {
-            "operationName": "PDPGetLayoutQuery",
-            "variables": {
-                "shopDomain": brand_uri,
-                "productKey": product_uri,
-                "layoutID": "",
-                "apiVersion": 1,
-                "tokonow": {
-                    "shopID": "0",
-                    "whID": "0",
-                    "serviceType": "",
+        data = [
+            {
+                "operationName": "PDPGetLayoutQuery",
+                "variables": {
+                    "shopDomain": brand_uri,
+                    "productKey": product_uri,
+                    "layoutID": "",
+                    "apiVersion": 1,
+                    "tokonow": {"shopID": "0", "whID": "0", "serviceType": ""},
+                    "extParam": "src%3Dshop",
                 },
-                "extParam": "src%3Dshop",
-            },
-            "query": get_product_details_query(),
-        },
-    ]
+                "query": get_product_details_query(),
+            }
+        ]
 
-    response = requests.post(PDP_REQUEST_URI, headers=headers, json=data)
-    return response.json()
+        response = requests.post(
+            PDP_REQUEST_URI, headers=headers, json=data, timeout=REQUEST_TIMEOUT
+        )
+        response.raise_for_status()
+        return response.json()
+    except (RequestException, ValueError) as e:
+        logger.error(f"Error retrieving product details for {product_url}: {str(e)}")
+        return None
+
+
+def get_image(image_url, brand_uri=""):
+    try:
+        response = requests.get(
+            image_url, headers=get_headers(brand_uri), timeout=REQUEST_TIMEOUT
+        )
+        response.raise_for_status()
+        return response.content
+    except (RequestException, ValueError) as e:
+        logger.error(f"Error retrieving image from {image_url}: {str(e)}")
+        return None
