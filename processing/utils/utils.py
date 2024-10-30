b8f11c741ce4252f642a478aaa62fb59c37bc941
Kenji Laurens <kenjilaurens@Kenjis-MacBook-Air.local>
2024-10-30 20:21:47 +0700

    upload to gcp functions

--- processing/utils/utils.py
+++ processing/utils/utils.py
@@ -2,7 +2,11 @@ import json
 import os
 import re
 from ..logger import logger
+from google.cloud import storage
 
+
+BUCKET_NAME = os.getenv("BUCKET_NAME")
+OVERWRITE_CLOUD = os.getenv("OVERWRITE_CLOUD") == "True"
 
 # MAGIC_NUMBERS = {
 #     b"\xFF\xD8": "jpg",
@@ -14,12 +18,14 @@ def create_dir_if_not_exists(directory: str):
     """Create a directory if it doesn't exist."""
     os.makedirs(directory, exist_ok=True)
 
+
 def save_json(data: dict, file_path: str):
     """Saves data to a JSON file."""
     create_dir_if_not_exists(os.path.dirname(file_path))
     with open(file_path, "w") as write_file:
         json.dump(data, write_file)
     logger.info(f"Saved data to {file_path}")
+
 
 def sanitize_product_name(product_name):
     sanitized_name = re.sub(r"[\n\t\/]+", " ", product_name).strip()
@@ -43,4 +49,30 @@ def sanitize_product_name(product_name):
 #                 break
 
 
-## define GCP upload function here
+def write_gcs(file_path, content):
+    """Uploads content to Google Cloud Storage."""
+    client = storage.Client()
+    bucket = client.bucket(BUCKET_NAME)
+    blob = bucket.blob(file_path)
+
+    # Write content to the blob
+    blob.upload_from_string(content)
+    logger.info(f"Written to GCS: {file_path}")
+
+
+def upload_to_gcs(file_path, bucket):
+    """Uploads a single file to GCP"""
+    client = storage.Client()
+    bucket = client.bucket(BUCKET_NAME)
+    try:
+
+        # Create a new blob (object) in the bucket
+        blob = bucket.blob(file_path)
+
+        if not blob.exists() or OVERWRITE_CLOUD:
+            # Upload the file
+            blob.upload_from_filename(file_path)
+            logger.info(f"Uploaded {file_path} to gs://{BUCKET_NAME}/{file_path}")
+
+    except Exception as e:
+        logger.error(f"Failed to upload {file_path}: {str(e)}")
